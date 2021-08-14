4f455977ed087aa87d91f30f23bc487ab705ede6
KNagaVivek <karnatinagavivek@gmail.com>
2021-08-14 19:41:24 +0530

    second

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'Hospital_Management.wsgi.application'
 #     }
 # }
 
-DATABASE = {
+DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
@@ -93,7 +93,7 @@ DATABASE = {
 }
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
